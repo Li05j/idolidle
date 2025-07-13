ea42b27c8651029fe2bcc874c15007152e1327c1
Jason <j989li@uwaterloo.ca>
2025-07-13 00:01:58 -0400

    added some dummy locations and actions

--- src/lib/stores/flag_update_handler.svelte.ts
+++ src/lib/stores/flag_update_handler.svelte.ts
@@ -15,19 +15,34 @@ class ProgressHandlers {
         return TD_List_Tracker;
     }
 
-    public handlers = new Map<ProgressKey, ProgressHandler>([
-        [`0-${this._pf.f0}`, () => {
-            let t = actions_data.get('Living Room');
+    private _get_new_locations(loc_names: string[]) {
+        loc_names.forEach((name) => {
+            let t = locations_data.find((ld) => ld.name === name);
             if (t) {
-                TD_List_Tracker.actions = new Map([
-                    ...TD_List_Tracker.actions,
-                    ['Living Room', t],
-                ]);
+                TD_List_Tracker.locations.push(t)
             }
-        }],
+        })
+    }
+
+    private _get_new_actions(loc_name: string) {
+        const index = TD_List_Tracker.locations.findIndex(ld => ld.name === loc_name);
+        if (index > -1) {
+            TD_List_Tracker.locations.splice(index, 1);
+        }
         
-        [`0-${this._pf.f1}`, () => { return }],
-        [`0-${this._pf.f2}`, () => { return }],
+        let t = actions_data.get(loc_name);
+        if (t) {
+            TD_List_Tracker.actions = new Map([
+                ...TD_List_Tracker.actions,
+                [loc_name, t],
+            ]);
+        }
+    }
+
+    public handlers = new Map<ProgressKey, ProgressHandler>([
+        [`0-${this._pf.f0}`, () => { this._get_new_actions('Living Room'); this._get_new_locations(['Park', 'School']) }],
+        [`0-${this._pf.f1}`, () => { this._get_new_actions('Park') }],
+        [`0-${this._pf.f2}`, () => { this._get_new_actions('School') }],
         [`0-${this._pf.f3}`, () => { return }],
     ]);
 }
--- src/lib/stores/todos_data.svelte.ts
+++ src/lib/stores/todos_data.svelte.ts
@@ -4,20 +4,45 @@ import { ProgressFlag } from '$lib/types'
 import { TD_List_Tracker } from "$lib/stores/todos_list_tracker.svelte";
 import { fans, moni, sta, charm, presence, eloq, poise } from "$lib/stores/stats.svelte";
 
+const S_TO_MS = 1000
+
 export const locations_data: Todo[] = [
     {
-        name: 'Living Room', 
-        type: 'location', 
-        base_cost: 3000, 
+        name: 'Living Room',
+        type: 'location',
+        base_cost: 3 * S_TO_MS, 
         depends: [{ which_stat: "sta", effectiveness: 1.0 }],
         reward: () => {
             sta.base += 0.3
         },
         then: () => {
-            TD_List_Tracker.locations = TD_List_Tracker.locations.filter((ld) => { ld.name == 'Living Room'})
             Game_Progress.enable(0, ProgressFlag.f0);
         }
-    }
+    },
+    {
+        name: 'Park', 
+        type: 'location',
+        base_cost: 30 * S_TO_MS, 
+        depends: [{ which_stat: "sta", effectiveness: 1.0 }],
+        reward: () => {
+            sta.base += 3
+        },
+        then: () => {
+            Game_Progress.enable(0, ProgressFlag.f1);
+        }
+    },
+    {
+        name: 'School', 
+        type: 'location',
+        base_cost: 300 * S_TO_MS, 
+        depends: [{ which_stat: "sta", effectiveness: 1.0 }],
+        reward: () => {
+            sta.base += 30
+        },
+        then: () => {
+            Game_Progress.enable(0, ProgressFlag.f2);
+        }
+    },
 ];
 
 export const actions_data: Map<string, Todo[]> = new Map([
@@ -26,7 +51,7 @@ export const actions_data: Map<string, Todo[]> = new Map([
             {
                 name: 'Dance', 
                 type: 'action', 
-                base_cost: 5000,
+                base_cost: 5 * S_TO_MS,
                 depends: [
                     { which_stat: "sta", effectiveness: 0.25 },
                     { which_stat: "charm", effectiveness: 0.75 },
@@ -38,7 +63,63 @@ export const actions_data: Map<string, Todo[]> = new Map([
             {
                 name: 'Sing', 
                 type: 'action', 
-                base_cost: 5000,
+                base_cost: 5 * S_TO_MS,
+                depends: [
+                    { which_stat: "sta", effectiveness: 0.25 },
+                    { which_stat: "eloq", effectiveness: 0.75 },
+                ],
+                reward: () => {
+                    eloq.base += 0.5
+                },
+            },
+        ],
+    ],
+    ['Park', 
+        [
+            {
+                name: 'Dance', 
+                type: 'action', 
+                base_cost: 5 * S_TO_MS,
+                depends: [
+                    { which_stat: "sta", effectiveness: 0.25 },
+                    { which_stat: "charm", effectiveness: 0.75 },
+                ],
+                reward: () => {
+                    charm.base += 0.5
+                },
+            },
+            {
+                name: 'Sing', 
+                type: 'action', 
+                base_cost: 5 * S_TO_MS,
+                depends: [
+                    { which_stat: "sta", effectiveness: 0.25 },
+                    { which_stat: "eloq", effectiveness: 0.75 },
+                ],
+                reward: () => {
+                    eloq.base += 0.5
+                },
+            },
+        ],
+    ],
+    ['School', 
+        [
+            {
+                name: 'Dance', 
+                type: 'action', 
+                base_cost: 5 * S_TO_MS,
+                depends: [
+                    { which_stat: "sta", effectiveness: 0.25 },
+                    { which_stat: "charm", effectiveness: 0.75 },
+                ],
+                reward: () => {
+                    charm.base += 0.5
+                },
+            },
+            {
+                name: 'Sing', 
+                type: 'action', 
+                base_cost: 5 * S_TO_MS,
                 depends: [
                     { which_stat: "sta", effectiveness: 0.25 },
                     { which_stat: "eloq", effectiveness: 0.75 },
